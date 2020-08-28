8ded47ccb56127317a612759f63b625078f547f8
Abhay <abhaykumar.lodha@gmail.com>
2020-08-28 14:31:14 +0530

    Few Additional Features

--- src/excercises/Sr_03_MyGenericListWithPredicates.scala
+++ src/excercises/Sr_03_MyGenericListWithPredicates.scala
@@ -9,21 +9,38 @@ object Sr_03_MyGenericListWithPredicates extends App {
    * isEmpty - returns true if the list is empty, else false.
    * add(int) - returns a new list with element added.
    * override toString - for String representation of the list.
-   * 
+   *
    * Extras :
-   * 1. Add a generic trait MyTrait[T] wit ha little method : test(T) => Boolean 
-   * 2. Add a generic trait MyTransformer[A, B] to Convert from A type list to B type.
+   * 1. Add a generic trait MyPredicate[T] with a little method : test(T) => Boolean
+   * 2. Add a generic trait MyTransformer[A, B] to Convert from A type to B type.
+   *    With a little method : transform(A) => B
    * 3. Add following function to List
    *     A. map((myTransformer) => List)
    *     B. filter(myPredicate => List)
-   *     C. flatMap(myTransFormer from A to List[B] => List[B]) 
-   *  
+   *     C. flatMap(myTransFormer from A to List[B] => List[B])
+   *
    * For Ex -
-   * If class EvenPredicate extends MyPredicate[Int], then test method will return if argumetn is even or not ?
-   * IF class StringToInt extends MyTransformer[String, Int] 
+   * If class EvenPredicate extends MyPredicate[Int], then test method will return if argument is even or not ?
+   * IF class StringToInt extends MyTransformer[String, Int], then transform method will transform List[A] to List[B]
+   *
+   * [1, 2, 3].map(n=>n * 2) should yield [2, 4, 6]
+   * [1, 2, 3].filter(n => n%2==0) should yield [2]
+   * [1, 2, 3].
    */
 
-  abstract class MyGenericList[+A] {
+  trait MyPredicate[+T] {
+    def test(a: T => Boolean): Boolean
+  }
+
+  trait MyFilter[+T] extends MyPredicate[T] {
+    def filter(test: T => Boolean): MyGenericList[T]
+  }
+
+  trait MyTransformer[+A] {
+    def map[B](a: A => B): MyGenericList[B]
+  }
+
+  abstract class MyGenericList[+A] extends MyFilter[A] with MyTransformer[A] {
     def head: A
 
     def tail: MyGenericList[A]
@@ -31,6 +48,8 @@ object Sr_03_MyGenericListWithPredicates extends App {
     def isEmpty: Boolean
 
     def add[B >: A](elem: B): MyGenericList[B]
+
+    def map[B](a: A => B): MyGenericList[B]
 
     def elementToString: String
 
@@ -44,7 +63,12 @@ object Sr_03_MyGenericListWithPredicates extends App {
 
     def isEmpty: Boolean = true
 
-    def add[B >: Nothing](elem: B): MyGenericList[B] = new Cons(elem, this)
+    def add[B >: Nothing](elem: B): MyGenericList[B] = new Cons(elem, Empty)
+
+    def test(predicate: Nothing => Boolean) = true
+    def filter(test: Nothing => Boolean): MyGenericList[Nothing] = Empty
+
+    def map[B](transform: Nothing => B): Nothing = throw new NoSuchElementException
 
     override def elementToString: String = s""
   }
@@ -57,6 +81,17 @@ object Sr_03_MyGenericListWithPredicates extends App {
     def isEmpty: Boolean = false
 
     def add[B >: A](elem: B): MyGenericList[B] = new Cons(elem, this)
+
+    def test(predicate: A => Boolean) = predicate(e)
+    def filter(test: A => Boolean): MyGenericList[A] = tailElem match {
+      case Empty => if (test(e)) new Cons(e, Empty) else Empty
+      case _ => if (test(e)) new Cons(e, tailElem.filter(test)) else tailElem.filter(test)
+    }
+
+    def map[B](transform: A => B): MyGenericList[B] = tailElem match {
+      case Empty => new Cons(transform(e), Empty)
+      case _ => new Cons(transform(e), tailElem.map(transform))
+    }
 
     override def elementToString: String = tailElem match {
       case Empty => s"$e"
@@ -75,5 +110,13 @@ object Sr_03_MyGenericListWithPredicates extends App {
   println(s"${l5.add(5)}")
   println(s"${l5.tail}")
   println(s"${l5.tail.head}")
+
+  val l6 = l5.map(a => a * 5)
+  val l7 = l5.filter(a => a % 2 == 0)
+
+  println("l5 : " + l5)
+  println("l6 : " + l6)
+  println("l7 : " + l7)
+
 }
 
